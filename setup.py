d12a635324cdc3c5ee7d0c81bcfd711957afb130
David Law <physical256@gmail.com>
2023-09-06 12:44:36 +0100

    changed classifiers

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
     classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Astronomers',
-    'Topic :: Astronomy :: unit and coorinate transformations',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.6',
