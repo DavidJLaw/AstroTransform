8361bdf24c22698f2577ca7fa63c83cfe04a0709
David Law <physical256@gmail.com>
2023-09-06 12:40:56 +0100

    added classifiers

--- setup.py
+++ setup.py
@@ -14,4 +14,15 @@ setup(
     description="Package for astronomical transformations and calculations.",
     long_description=open('README.md', 'r').read() if 'README.md' in os.listdir() else "",
     long_description_content_type="text/markdown",
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Astronomers',
+    'Topic :: Astronomy :: unit and coorinate transformations',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+],
 )
